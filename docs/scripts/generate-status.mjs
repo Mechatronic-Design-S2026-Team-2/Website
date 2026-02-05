7abb4deedb239e4d5d876b0de07eaa14867996a5
dgl7 <123701059+dgl7@users.noreply.github.com>
2026-02-05 13:42:35 -0500

    Refactor milestones query to include issue states

--- docs/scripts/generate-status.mjs
+++ docs/scripts/generate-status.mjs
@@ -171,9 +171,11 @@ query DashboardNoUpdates($org: String!, $robotRepo: String!, $projectNumber: Int
 
     milestones(first: 50, states: [OPEN]) {
       nodes {
-        title url dueOn
-        openIssues { totalCount }
-        closedIssues { totalCount }
+        title
+        url
+        dueOn
+        openIssues: issues(states: [OPEN]) { totalCount }
+        closedIssues: issues(states: [CLOSED]) { totalCount }
       }
     }
 
